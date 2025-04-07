a21dad71bb41f1ea1be1bc85e1fb921d3790d872
Longlasting1805 <Longlasting1805@gmail.com>
2025-04-07 09:54:47 +0100

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,37 @@
+import diamondlogo from './diamondlogo.jpg'
+import search_icon from './search_icon.png'
+import cart_icon from './cart_icon.png'
+import profile_icon from './profile_icon.jpg'
+
+export const assets = {
+    diamondlogo,
+    search_icon,
+    cart_icon,
+    profile_icon
+
+}
+
+export const products =[{
+    _id: "aaaaaa",
+    name: "Women Wrist Watch", 
+    description: "A lightweight wrist watch",
+    price: 500,
+    image: [], 
+    category: "women",
+    sizes: ["S", "M", "L"],
+    date: 15442556, 
+    bestseller: true
+
+}, 
+{
+    _id: "aaaaab",
+    name: "men Wrist Watch", 
+    description: "A lightweight men wrist watch",
+    price: 500,
+    image: [], 
+    category: "men",
+    sizes: ["M", "L", "XL"],
+    date: 15442556, 
+    bestseller: true
+
+}]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+      <div className="text-2xl font-bold p-6">
+       
+      </div>
+    );
+  };
+
+export default Home
