8cac11ed09e0e75049ec3e3c11216416e3e1fdbd
Longlasting1805 <Longlasting1805@gmail.com>
2025-04-10 13:04:15 +0100

    Space between latest collections

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -16,8 +16,8 @@ const LatestCollection = () => {
 
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+        <div className='text-center py-8 text-3xl '>
+            <Title text1={ 'LATEST'} text2={'COLLECTIONS'} />
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
                 This are new collections available for our new arrivals
             </p>
