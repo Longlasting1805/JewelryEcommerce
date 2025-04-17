890953b592f22ee6df843702f733f41b7391b2a1
Longlasting1805 <Longlasting1805@gmail.com>
2025-04-17 11:03:08 +0100

    cartTotal price completed

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 import { products } from "../assets/assets";
 import { toast } from 'react-toastify'
+import { useNavigate } from "react-router-dom";
 
 
 export const ShopContext = createContext();
@@ -14,6 +15,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async (itemId, size) => {
 
@@ -66,12 +68,30 @@ const ShopContextProvider = (props) => {
 
     }
 
+    const getCartAmount =  () => {
+        let totalAmount = 0
+        for(const items in cartItems){
+            let itemInfo = products.find((product) => product._id === items)
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems[items][item] > 0){
+                        totalAmount += itemInfo.price * cartItems[items][item]
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalAmount
+    }
+
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart, 
-        getCartCount, updateQuantity
+        getCartCount, updateQuantity, 
+        getCartAmount, navigate
 
 
     }
