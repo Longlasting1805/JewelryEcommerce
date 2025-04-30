8d271a2cd0ad09c85bfcadf5b2778a6b2b2eff5b
Longlasting1805 <Longlasting1805@gmail.com>
2025-04-30 19:39:48 +0100

    register api created

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
@@ -12,3 +13,7 @@ const productSchema = new mongoose.Schema({
     date: { type: Number, required: true }
 
 })
+
+const productModel = mongoose.models.product ||  mongoose.model("product", productSchema)
+
+export default productModel;
