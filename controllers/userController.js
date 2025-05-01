e3752c168a24cffd3607bb3efa7f7f228ddfe0a7
Longlasting1805 <Longlasting1805@gmail.com>
2025-05-01 01:45:44 +0100

    login api added

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,33 +4,57 @@ import jwt from 'jsonwebtoken'
 import userModel from "../models/userModel.js"
 
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET)
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 // route for login
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
 
-    
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exists" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({ succes: true, token })
+        }
+        else {
+            res.json({ success: false, message: 'Invalid credentials' })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+
+
+
 }
 
 // route for register
 const registerUser = async (req, res) => {
-    try{
+    try {
         const { name, email, password } = req.body
 
         //checking user already exist or not
-        const exists = await userModel.findOne({email})
+        const exists = await userModel.findOne({ email })
         if (exists) {
-            return res.json({success:false, message: "User already exists"})
+            return res.json({ success: false, message: "User already exists" })
         }
 
         // validating email format & strong password
-        if (!validator.isEmail(email)){
-            return res.json({success:false, message: "Please enter a valid email"})
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
         }
 
-        if (password.lenght < 8){
-            return res.json({success:false, message: "Please enter a strong password"})
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
         }
 
         // hashing user password
@@ -40,21 +64,21 @@ const registerUser = async (req, res) => {
         const newUser = new userModel({
             name,
             email,
-            password:hashedPassword
+            password: hashedPassword
         })
 
         const user = await newUser.save()
 
         const token = createToken(user._id)
 
-        res.json({success: true, token})
+        res.json({ success: true, token })
 
-         
 
-    } catch (error){
+
+    } catch (error) {
         console.log(error);
-        res.json({success:false, message: error.message})
-        
+        res.json({ success: false, message: error.message })
+
 
     }
 
@@ -63,7 +87,7 @@ const registerUser = async (req, res) => {
 // route for admin login
 const adminLogin = async (req, res) => {
 
-    
+
 }
 
-export {loginUser, registerUser, adminLogin}
+export { loginUser, registerUser, adminLogin }
